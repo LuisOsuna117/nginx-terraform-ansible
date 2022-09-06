4876a790734f6eeefa588e202bc17b062a49ae56
José Luis Osuna Moreno <joseosuna@solemti.com>
2022-09-05 19:15:17 -0600

    Minor fix

--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+##### Instance ID captured through Instance meta data #####
+InstanceID=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/instance-id`
+##### Region captured through Instance meta data #####
+InstanceRegion=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/placement/region`
+##### Set a tag name indicating instance is not configured ####
+aws ec2 create-tags --region $InstanceRegion --resources $InstanceID --tags Key=Initialized,Value=false
 ##### Install Ansible ######
 yum update -y
 yum install git amazon-cloudwatch-agent -y
@@ -9,3 +15,5 @@ cd nginx-terraform-ansible
 ##### RUN ansible playbook #####
 ansible-playbook ./ansible/nginx/nginx_install.yml
 ansible-playbook ./ansible/cloudwatch/cloudwatch-config.yml
+##### Update TAG ######
+aws ec2 create-tags --region $InstanceRegion --resources $InstanceID --tags Key=Initialized,Value=true
